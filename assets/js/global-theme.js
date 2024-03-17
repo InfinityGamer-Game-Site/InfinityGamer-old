c3cf07c6bdc4305a8d740dc35cf37cd65e511ed3
InfinityLoop <127774659+InfinityLoop1@users.noreply.github.com>
2024-03-17 19:03:19 +0000

    Fixed some stuff with navbar and theme

--- assets/js/global-theme.js
+++ assets/js/global-theme.js
@@ -1,15 +1,21 @@
 function toggleTheme(theme) {
-    const body = document.body;
-    body.className = theme;
-    localStorage.setItem('theme', theme);
+  const body = document.body;
+  body.className = theme;
+  localStorage.setItem("theme", theme);
+  location.reload();
+}
+function loadTheme(theme) {
+  const body = document.body;
+  body.className = theme;
+  localStorage.setItem("theme", theme);
 }
 
 // Check if theme is saved in local storage
-const savedTheme = localStorage.getItem('theme');
+const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme) {
-    toggleTheme(savedTheme);
+  loadTheme(savedTheme);
 } else {
-    // Default to dark theme if no theme is saved
-    toggleTheme('dark');
+  // Default to dark theme if no theme is saved
+  toggleTheme("dark");
 }
