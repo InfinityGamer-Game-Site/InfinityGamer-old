88ae8f61a17e51431d9bd0f007260495ab9eac19
InfinityLoop <127774659+InfinityLoop1@users.noreply.github.com>
2024-05-23 20:30:09 -0500

    whoopsie

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -62,3 +62,35 @@ if (localStorage.getItem('autoAboutBlankEmbed') === 'true') {
 } else {
   document.getElementById('aabeSwitch').checked = false;
 }
+
+document.getElementById('backupBtn').addEventListener('click', () => {
+  const localStorageData = JSON.stringify(localStorage);
+  const blob = new Blob([localStorageData], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+  
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'localStorageBackup.infinitygamersave';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+});
+
+document.getElementById('restoreBtn').addEventListener('click', () => {
+  document.getElementById('restoreFileInput').click();
+});
+
+document.getElementById('restoreFileInput').addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+          const data = JSON.parse(e.target.result);
+          for (const key in data) {
+              localStorage.setItem(key, data[key]);
+          }
+          alert('LocalStorage restored successfully!');
+      };
+      reader.readAsText(file);
+  }
+});
