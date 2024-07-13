79468c93e4ad6ddd98fd05c1778b40b7e9f0cd01
InfinityLoop <127774659+InfinityLoop1@users.noreply.github.com>
2024-07-12 21:10:46 -0500

    i think i fixed it

--- r.ps1
+++
@@ -1,9 +0,0 @@
-# Remove all files tracked by Git LFS
-$lfsFiles = git lfs ls-files
-
-foreach ($line in $lfsFiles.Split([System.Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)) {
-    $file = ($line -split '\s+')[2].Trim()
-    Write-Output "Removing LFS tracking for $file"
-    git lfs untrack "$file"
-    git rm --cached "$file"
-}
